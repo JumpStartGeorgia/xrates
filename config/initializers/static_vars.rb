5567412ad7fe2361630f9ceb4f2dbe92f2c2d478
jason <jason.addie@gmail.com>
2015-04-14 15:00:30 +0400

    changed last scrape constant to use created_at date

--- config/initializers/static_vars.rb
+++ config/initializers/static_vars.rb
@@ -1,4 +1,4 @@
 CURRENCIES = Currency.with_translations(:en).map{|x| x.code }
 BANKS = Bank.with_translations(:en).map{|x| x.id.to_s }
 
-LAST_SCRAPE = Rate.maximum(:utc)
+LAST_SCRAPE = Rate.maximum(:created_at)
