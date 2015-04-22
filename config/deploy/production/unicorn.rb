902a737943d77d8e49725c5d3175313e7e83117f
jason <jason.addie@gmail.com>
2015-04-22 10:04:00 +0400

    added prod deploy

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,9 +1,9 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/bootstrap/Bootstrap-Starter/current" # path to application current folder
-sock_name = "unicorn_Bootstrap-Starter" # must be unique name with no '.'
-port_num = 8091 # must be a unique port number for this application
+root = "/home/lari/Lari-Explorer/current" # path to application current folder
+sock_name = "unicorn_Lari-Explorer" # must be unique name with no '.'
+port_num = 8136 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
